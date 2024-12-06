09b82fc23c1efa6a516a82efffe2781649f6f4e7
Kushagra Sinha <sinhak@google.com>
2024-12-06 15:07:05 +0100

    Solve Day 4 Part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -3,11 +3,13 @@ package dev.sinhak.aoc2024.day04
 import println
 import readInput
 
+const val X_SEARCH_STRING = "MAS"
+
 fun main() {
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 0L)
-    check(part2(testInput) == 0L)
+    check(part2(testInput) == 9L)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day04")
@@ -27,12 +29,36 @@ fun part1(input: List<String>): Long {
 }
 
 fun part2(input: List<String>): Long {
-    return 0L
+    var numMatches = 0L
+    for (i in 0..<input.size) {
+        for (j in 0..<input[i].length) {
+            // Start searching for X-MASes starting at this position.
+            numMatches += findXMas(input, i, j)
+        }
+    }
+
+    return numMatches
 }
 
-/** Returns the number of matches we can find for [SEARCH_STRING] in [input] starting at (row, col)
- * with matching in [SEARCH_STRING] starting at [index] */
-fun findMatch(input: List<String>, searchString: String, row: Int, col: Int, index: Int = 0): Int {
+fun findXMas(input: List<String>, row: Int, col: Int): Long {
+    val upRight = findMatch(input, X_SEARCH_STRING, row, col, -1, 1, 0)
+    val downRight = findMatch(input, X_SEARCH_STRING, row, col, 1, 1, 0)
+    val downLeft = findMatch(input, X_SEARCH_STRING, row, col, 1, -1, 0)
+
+    val upRightMatches = upRight * findMatch(input, X_SEARCH_STRING, row, col + 2, -1, -1, 0)
+
+    val downRightMatches = downRight * findMatch(input, X_SEARCH_STRING, row + 2, col, -1, 1, 0) +
+            downRight * findMatch(input, X_SEARCH_STRING, row, col + 2, 1, -1, 0)
+
+    val downLeftMatches = downLeft * findMatch(input, X_SEARCH_STRING, row + 2, col, -1, -1, 0)
+
+    val total = upRightMatches + downRightMatches + downLeftMatches
+    return total
+}
+
+/** Returns the number of matches we can find for [searchString] in [input] starting at (row, col)
+ * with matching in [searchString] starting at [index] */
+fun findMatch(input: List<String>, searchString: String, row: Int, col: Int, index: Int = 0): Long {
     if (index >= searchString.length) {
         return 0
     }
@@ -54,7 +80,7 @@ fun findMatch(input: List<String>, searchString: String, row: Int, col: Int, ind
         return 1
     }
 
-    var numMatches = 0
+    var numMatches = 0L
     for (rowDelta in -1..1) {
         for (colDelta in -1..1) {
             numMatches += findMatch(input, searchString, row + rowDelta, col + colDelta, rowDelta, colDelta, index + 1)
@@ -69,7 +95,7 @@ fun findMatch(input: List<String>, searchString: String, row: Int, col: Int, ind
  * by (rowDelta, colDelta). */
 fun findMatch(
     input: List<String>, searchString: String, row: Int, col: Int, rowDelta: Int, colDelta: Int, index: Int
-): Int {
+): Long {
     if (index >= searchString.length) {
         return 0
     }

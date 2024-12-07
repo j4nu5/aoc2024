aa8539239d9f0ca59c7562aa1848aea54d2194a0
Kushagra Sinha <sinhak@google.com>
2024-12-07 13:35:26 +0100

    Day 7 - Use Long instead of BigInteger

--- src/Day07.kt
+++ src/Day07.kt
@@ -2,30 +2,29 @@ package dev.sinhak.aoc2024.day07
 
 import println
 import readInput
-import java.math.BigInteger
 
 data class Equation(
-    val target: BigInteger,
-    val operands: List<BigInteger>,
+    val target: Long,
+    val operands: List<Long>,
 )
 
 fun main() {
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("3: 3")) == BigInteger.valueOf(3L))
-    check(part1(listOf("9876: 6 6 823")) == BigInteger.valueOf(9876L))
-    check(part1(listOf("4: 1 1 4 2 3")) == BigInteger.valueOf(0L))
+    check(part1(listOf("3: 3")) == 3L)
+    check(part1(listOf("9876: 6 6 823")) == 9876L)
+    check(part1(listOf("4: 1 1 4 2 3")) == 0L)
 
     val testInput = readInput("Day07_test")
-    check(part1(testInput) == BigInteger.valueOf(3749L))
-    check(part2(testInput) == BigInteger.valueOf(11387))
+    check(part1(testInput) == 3749L)
+    check(part2(testInput) == 11387L)
 
     val input = readInput("Day07")
     part1(input).println()
     part2(input).println()
 }
 
-fun part1(input: List<String>): BigInteger {
-    var totalCalibrationResult = BigInteger.ZERO
+fun part1(input: List<String>): Long {
+    var totalCalibrationResult = 0L
 
     for (line in input) {
         val equation: Equation = parse(line)
@@ -37,8 +36,8 @@ fun part1(input: List<String>): BigInteger {
     return totalCalibrationResult
 }
 
-fun part2(input: List<String>): BigInteger {
-    var totalCalibrationResult = BigInteger.ZERO
+fun part2(input: List<String>): Long {
+    var totalCalibrationResult = 0L
 
     for (line in input) {
         val equation: Equation = parse(line)
@@ -52,12 +51,12 @@ fun part2(input: List<String>): BigInteger {
 
 fun canBeSatisfied(
     equation: Equation,
-    findPossibleEvaluations: (BigInteger, BigInteger) -> List<BigInteger>,
+    findPossibleEvaluations: (Long, Long) -> List<Long>,
 ): Boolean {
-    var possibleTargets = mutableListOf<BigInteger>()
+    var possibleTargets = mutableListOf<Long>()
 
     if (equation.operands.isEmpty()) {
-        return equation.target == BigInteger.ZERO
+        return equation.target == 0L
     }
 
     if (equation.operands[0] == equation.target) {
@@ -67,10 +66,10 @@ fun canBeSatisfied(
     possibleTargets.add(equation.operands[0])
     for (i in 1..<equation.operands.size) {
         val operand = equation.operands[i]
-        val newPossibleTargets = mutableListOf<BigInteger>()
+        val newPossibleTargets = mutableListOf<Long>()
 
         for (target in possibleTargets) {
-            val newTargets: List<BigInteger> = findPossibleEvaluations(target, operand)
+            val newTargets: List<Long> = findPossibleEvaluations(target, operand)
             newPossibleTargets.addAll(newTargets.filter { !shouldBePruned(it, equation.target) })
         }
 
@@ -80,33 +79,33 @@ fun canBeSatisfied(
     return possibleTargets.contains(equation.target)
 }
 
-fun findPossibleEvaluationsWithAddAndMultiplication(target: BigInteger, operand: BigInteger): List<BigInteger> {
+fun findPossibleEvaluationsWithAddAndMultiplication(target: Long, operand: Long): List<Long> {
     return listOf(target + operand, target * operand)
 }
 
 fun findPossibleEvaluationsWithAddMultiplicationAndConcat(
-    target: BigInteger, operand: BigInteger
-): List<BigInteger> {
+    target: Long, operand: Long
+): List<Long> {
     return listOf(target + operand, target * operand, concat(target, operand))
 }
 
-fun concat(a: BigInteger, b: BigInteger): BigInteger {
+fun concat(a: Long, b: Long): Long {
     var result = a
     b.toString().forEach { ch ->
-        result = result * BigInteger.TEN + BigInteger.valueOf((ch - '0').toLong())
+        result = result * 10 + (ch - '0').toLong()
     }
     return result
 }
 
-fun shouldBePruned(possibleTarget: BigInteger, target: BigInteger): Boolean {
+fun shouldBePruned(possibleTarget: Long, target: Long): Boolean {
     return possibleTarget > target
 }
 
 fun parse(input: String): Equation {
     val targetAndOperandsSplit = input.split(":")
     check(targetAndOperandsSplit.size == 2)
-    val target = targetAndOperandsSplit[0].toBigInteger()
+    val target = targetAndOperandsSplit[0].toLong()
 
-    val operators: List<BigInteger> = targetAndOperandsSplit[1].trim().split(" ").map { it.toBigInteger() }
+    val operators: List<Long> = targetAndOperandsSplit[1].trim().split(" ").map { it.toLong() }
     return Equation(target, operators)
 }

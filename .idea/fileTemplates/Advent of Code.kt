05621af7024a8e80f04b2108774f878af8e73d17
Kushagra Sinha <sinhak@google.com>
2024-12-08 00:31:49 +0100

    Fix the template

--- .idea/fileTemplates/Advent of Code.kt
+++ .idea/fileTemplates/Advent of Code.kt
@@ -1,16 +1,25 @@
 #set( $Code = "bar" )
+package dev.sinhak.aoc2024.day$Day
+
+import println
+import readInput
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    check(part1(listOf("3 5")) == 0L)
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    val testInput = readInput("Day${Day}_test")
+    check(part1(testInput) == 0L)
+    check(part2(testInput) == 0L)
 
-    check(part1(listOf("...")) == 1)
-
-    val input = readInput("Day$Day")
+    val input = readInput("Day${Day}")
     part1(input).println()
     part2(input).println()
+}
+
+fun part1(input: List<String>): Long {
+    return 0L
+}
+
+fun part2(input: List<String>): Long {
+    return 0L
 }

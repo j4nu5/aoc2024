b562d27c512eefd59f21c2c248592d85e82b1f8f
Kushagra Sinha <sinhak@google.com>
2024-12-07 17:43:48 +0100

    Day 7 - A Star Search

--- src/Day07.kt
+++ src/Day07.kt
@@ -2,6 +2,8 @@ package dev.sinhak.aoc2024.day07
 
 import println
 import readInput
+import java.util.PriorityQueue
+import kotlin.math.abs
 import kotlin.math.pow
 import kotlin.time.measureTimedValue
 
@@ -31,6 +33,9 @@ fun main() {
     val bfsWithPruningAndOptAllocations = fun(equation: Equation): Boolean {
         return canBeSatisfiedWithOptAllocations(equation)
     }
+    val aStar = fun(equation: Equation): Boolean {
+        return canBeSatisfiedWithAStarSearch(equation)
+    }
 
     println("\nBenchmarks -\n")
     benchmark("Original recipe - BFS with pruning", curry(input, fun(input: List<String>): Long {
@@ -39,6 +44,11 @@ fun main() {
     benchmark(
         "Extra crunchy - BFS with pruning with optimized allocations", curry(input, fun(input: List<String>): Long {
             return part2(input, bfsWithPruningAndOptAllocations)
+        })
+    )
+    benchmark(
+        "Flaming hot - A Star search", curry(input, fun(input: List<String>): Long {
+            return part2(input, aStar)
         })
     )
 }
@@ -157,6 +167,13 @@ fun canBeSatisfiedWithOptAllocations(
     return possibleTargets.contains(equation.target)
 }
 
+fun canBeSatisfiedWithAStarSearch(
+    equation: Equation,
+): Boolean {
+    val solver = AStarSolver(equation)
+    return solver.isSolutionPossible()
+}
+
 fun findPossibleEvaluationsWithAddAndMultiplication(target: Long, operand: Long): List<Long> {
     return listOf(target + operand, target * operand)
 }
@@ -186,4 +203,109 @@ fun parse(input: String): Equation {
 
     val operators: List<Long> = targetAndOperandsSplit[1].trim().split(" ").map { it.toLong() }
     return Equation(target, operators)
+}
+
+class AStarSolver(private val equation: Equation) {
+    // We are going to use A Star search to see if equation can be solved.
+    // The idea is to construct a search tree of possible evaluation results by iteratively moving
+    // through the list of operands in equation.
+    // The halting criteria will be either finding an evaluation result as a leaf node (note the
+    // difference from standard A Star implementation - we cannot end the search early) - or by
+    // exhausting the search space.
+
+    // Represents a node in the search space.
+    inner class Node(
+        // Value / result of the evaluation which produced this node.
+        val value: Long,
+
+        // Level of node in the search tree. Range - [0, number of operands), where 0 is root.
+        val level: Int,
+    ) : Comparable<Node> {
+        fun isLeafNode(): Boolean {
+            return level == (equation.operands.size - 1)
+        }
+
+        fun isValidSolution(): Boolean {
+            return (value == equation.target) && isLeafNode()
+        }
+
+        override fun compareTo(other: Node): Int {
+            val cmp = estimatedCost() - other.estimatedCost()
+            return when {
+                cmp == 0L -> {
+                    0
+                }
+
+                cmp < 0L -> {
+                    -1
+                }
+
+                else -> {
+                    1
+                }
+            }
+        }
+
+        private fun estimatedCost(): Long {
+            // Lower the better.
+
+            // Unlike regular A star search, we are not trying to the shortest path. We have to
+            // end up at the leaf node anyway. However, it may be useful to prune out
+            // non-promising search branches as early as possible.
+
+            // If delta is our heuristic value -
+            // Low delta, low level = Good.
+            // Low delta, high level = Perfect.
+            // High delta, low level = OK.
+            // High delta, high level = Bad.
+
+            // TODO: Find a better formula to represent the above idea.
+            return level + heuristic()
+        }
+
+        private fun heuristic(): Long {
+            return abs(value - equation.target)
+        }
+    }
+
+    // Represents the frontier of the search space.
+    val frontier = PriorityQueue<Node>()
+
+    fun isSolutionPossible(): Boolean {
+        if (equation.operands.isEmpty()) {
+            return equation.target == 0L
+        }
+
+        frontier.add(
+            Node(
+                value = equation.operands[0],
+                level = 0,
+            )
+        )
+        while (frontier.isNotEmpty()) {
+            val currentNode = frontier.poll()
+
+            if (currentNode.isValidSolution()) {
+                return true
+            }
+
+            if (currentNode.isLeafNode()) {
+                continue
+            }
+
+            // We need to try the "next" operand on [currentNode].
+            val operand = equation.operands[currentNode.level + 1]
+            val newPossibleTargets: List<Long> =
+                findPossibleEvaluationsWithAddMultiplicationAndConcat(currentNode.value, operand)
+
+            frontier.addAll(newPossibleTargets.filter { !shouldBePruned(it, equation.target) }.map {
+                Node(
+                    value = it,
+                    level = currentNode.level + 1,
+                )
+            })
+        }
+
+        return false
+    }
 }
